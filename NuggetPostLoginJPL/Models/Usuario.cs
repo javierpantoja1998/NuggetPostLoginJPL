116b4f66b696ad53bf34a7fd7e68770aaf470722
javierpantoja1998 <cientificodebanco@gmail.com>
2023-05-16 10:06:11 +0200

    Add project files.

---
+++ NuggetPostLoginJPL/Models/Usuario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuggetPostLoginJPL.Models
+{
+    [Table("USUARIOS")]
+    public class Usuario
+    {
+        [Key]
+        [Column("IDUSUARIO")]
+        public int IdUsuario { get; set; }
+
+        [Column("NOMBRE")]
+        public string Nombre { get; set; }
+
+        [Column("DNI")]
+        public string Dni { get; set; }
+
+        [Column("DIRECCION")]
+        public string Direccion { get; set; }
+
+        [Column("TELEFONO")]
+        public string Telefono { get; set; }
+
+        [Column("EMAIL")]
+        public string Email { get; set; }
+
+        [Column("SALT")]
+        public string Salt { get; set; }
+
+        [Column("PASSWORD")]
+        public byte[] Password { get; set; }
+    }
+}
